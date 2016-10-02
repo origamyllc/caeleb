38ba49eaea12ad3e2bed6031b3e12b3e93ee80cd
prashunjaveri@gmail.com <prashun@origamyinc.com>
2016-10-02 15:32:46 -0700

    fixing rabbitmq #1,#6,#7

--- src/rabbitmqEngine.js
+++ src/rabbitmqEngine.js
@@ -37,90 +37,66 @@ else{
 }
 
 const  connection = amqp.createConnection(options);
-
-   let exchanges = {};
-   let queues = {};
-   let _queues = [];
+let exchanges = {};
+let queues = {};
+let _queues = {};
 
     class rabbitmqEngine extends abstactPublishSubscribeEngine {
 
-        init(){
-            connection.on('ready', () => {
-                // puts("connected to " + connection.serverProperties.product);
-                // There is no need to declare type, 'topic' is the default:
-                Object.keys(exchanges).forEach((exchange_name) => {
-
-                    let _exchange = exchanges[exchange_name];
-
-                    // Consumer:
-                    Object.keys(queues).forEach((key) => {
-
-                        let _queues = null;
-
-                        if(key === exchange_name){
-                            _queues = queues[key]
-                        }
-
-                        _queues.forEach(function(queue){
-
-                            let q  = connection.queue(queue.name);
-                            q.bind(_exchange.name, queue.routing_key);
-
-                        });
-                    });
-                });
-            });
+        init(exchanges,queues){
+            let exchange_key;
+            for (exchange_key in exchanges ){
+                let key;
+                let _queue = queues[exchange_key];
+                for(  key in _queue ){
+                    let queue = _queue[key];
+                    let q = connection.queue(queue.name);
+                    q.bind(exchange_key, queue.routing_key);
+                }
+            }
         }
 
         createExchange(name, options) {
-            return new Promise((resolve) => {
-                connection.on('ready', function () {
-                    exchanges[name] = connection.exchange(name, options, (exchange) => {
-                        resolve(exchange.name);
-                    });
-                });
+            return new Promise( (resolve) => {
+                exchanges[name] = connection.exchange(name, options,  (exchange) => {});
+                resolve(exchanges);
             });
         }
 
         registerQueue(queue_name, exchange_name, routing_key) {
-         return new Promise(function (resolve) {
-                    let value = { name: queue_name, routing_key: routing_key };
-                    let key = exchange_name;
-                    let obj = {};
-                    _queues[ value.name ] = value;
-                    queues[key] = _queues;
-                    resolve(queues);
-                });
+            return new Promise( (resolve) => {
+                let value = { name: queue_name, routing_key: routing_key };
+                let key = exchange_name;
+                _queues[value.name] = value;
+                queues[key] = _queues;
+                resolve(queues);
+            });
         }
 
         publish(routing_key, body) {
             return new Promise((resolve) => {
-                connection.on('ready', () => {
-                    connection.publish(routing_key, body, null , () => {
-                       // console.log('publish')
-                    })
-                })
-                resolve(body);
+                    connection.publish(routing_key, body, null, () => {
+                        // console.log('publish')
+                        resolve(body);
+                    });
+
             });
         }
 
         subscribe(queue_name,routing_key) {
-            return new Promise((resolve) => {
-                // Wait for connection to become established.
-                connection.on('ready', function () {
+            return new Promise( (resolve) => {
                     // Use the default 'amq.topic' exchange
-                    connection.queue(queue_name, function (q) {
+                    connection.queue(queue_name,  (q) => {
                         // Catch all messages
                         q.bind(routing_key);
 
                         // Receive messages
-                        q.subscribe(function (message) {
+                        q.subscribe( (message) => {
                             // Print messages to stdout
                             resolve(message);
                         });
                     });
                 });
-            });
         }
     }
 
